9c8b47082c24be9f39414818066fe86072cc263e
Farah <farahaladwanii24@gmail.com>
2023-02-14 18:25:05 +0300

    new version

--- challenge2.dart
+++ challenge2.dart
@@ -6,10 +6,14 @@
  * e.g.
  * greet("Hamza") => prints "Hello Hamza"
  */
-void main(){
-  greet()
+void main() {
+  greet(name: "Hamza");
+  print(isOdd(3));
+  print(squareOrDouble(3));
+  print(oddsSmallerThan(7));
 }
-void greet(String name) {
+
+void greet({String name = ""}) {
   print("Hello $name");
   // Your code here
 }
@@ -24,32 +28,49 @@ void greet(String name) {
  * isOdd(10) -> false
  */
 bool isOdd(int n) {
+  if (n % 2 != 0) {
+    return true;
+  } else {
+    return false;
+  }
   // Your code here
 }
 
-/**
- * oddsSmallerThan(n):
- * - receives a number n,
- * - returns the number of ODD numbers smaller than n.
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
+// /**
+//  * oddsSmallerThan(n):
+//  * - receives a number n,
+//  * - returns the number of ODD numbers smaller than n.
+//  *
+//  * e.g.
+//  * oddsSmallerThan(7) -> 3
+//  * oddsSmallerThan(15) -> 7
+//  */
 int oddsSmallerThan(int n) {
-  // Your code here
+  int number = 1;
+  int counter = 0;
+  while (number < n) {
+    if (number % 2 == 1) {
+      counter += 1;
+    }
+    number++;
+  }
+  return counter;
 }
 
-/**
- * squareOrDouble(n):
- * - receives a number n,
- * - returns n's square if n is odd,
- * - returns n's double if n is even.
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
- */
+// /**
+//  * squareOrDouble(n):
+//  * - receives a number n,
+//  * - returns n's square if n is odd,
+//  * - returns n's double if n is even.
+//  *
+//  * e.g.
+//  * squareOrDouble(16) -> 32
+//  * squareOrDouble(9) -> 81
+//  */
 int squareOrDouble(int n) {
-  // Your code here
+  if (n % 2 == 0) {
+    return n + n;
+  } else {
+    return n * n;
+  }
 }
